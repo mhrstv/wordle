5f4947b5153ad03fc334bf4cb75bdff0f9b7fcb0
mhrstv <mhrstvv@gmail.com>
2025-12-16 23:50:20 +0200

    feat: add project details

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+/*
+*
+* Solution to course project "Wordle"
+* Introduction to Programming (Practicum) Course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2024/2025
+*
+* @author Martin Hritsov
+* @idnumber 4MI0600616
+* @compiler GCC
+*
+*/
+
 #include "headers/menu.h"
 
 #include <iostream>
