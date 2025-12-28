75ca0cb27242062b449c0ee49940ba9467d626e8
mhrstv <mhrstvv@gmail.com>
2025-12-28 15:00:41 +0200

    feat: add subStr and strCpy implementations

--- headers/std.hpp
+++ headers/std.hpp
@@ -18,6 +18,7 @@
 #pragma once
 inline int strLen(const char* str)
 {
+    if(!str) return 0;
     int len = 0;
     if(!str) return 0;
     while(str[len] != '\0')
@@ -29,6 +30,7 @@ inline int strLen(const char* str)
 
 inline bool strEquals(const char* str1, const char* str2)
 {
+    if(!str1 || !str2) return false;
     while(*str1 == *str2)
     {
         if(*str1 == '\0') return true;
@@ -36,4 +38,40 @@ inline bool strEquals(const char* str1, const char* str2)
         str2++;
     }
     return false;
+}
+
+inline char* subStr(const char* src, size_t start, size_t length) {
+    if(src == nullptr) return nullptr;
+    size_t src_len = strLen(src);
+    
+    if (start >= src_len) {
+        char* empty = new char[1];
+        empty[0] = '\0';
+        return empty;
+    }
+
+    if (start + length > src_len) {
+        length = src_len - start;
+    }
+
+    char* dest = new char[length + 1];
+
+    for (size_t i = 0; i < length; ++i) {
+        dest[i] = src[start + i];
+    }
+    
+    dest[length] = '\0';
+    return dest;
+}
+
+inline void strCpy(char* dest, const char* src)
+{
+    if (dest == nullptr || src == nullptr) return;
+    int i = 0;
+    while(src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
 }
